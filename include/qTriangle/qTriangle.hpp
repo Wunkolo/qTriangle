db0b981d63c0dbd2d97c3fbf4541cbc21821ad31
Wunkolo <Wunkolo@gmail.com>
2018-06-04 17:21:24 -0400

    Add BarycentricFill algorithm

--- include/qTriangle/qTriangle.hpp
+++ include/qTriangle/qTriangle.hpp
@@ -12,7 +12,7 @@ namespace qTri
 extern const std::pair<
 	void(*)(qTri::Image& Frame, const qTri::Triangle& Tri),
 	const char*
-> FillAlgorithms[2];
+> FillAlgorithms[3];
 
 bool CrossTest(const qTri::Vec2& Point, const qTri::Triangle& Tri);
 bool Barycentric(const qTri::Vec2& Point, const qTri::Triangle& Tri);
--- source/qTriangle/qTriangle.cpp
+++ source/qTriangle/qTriangle.cpp
@@ -43,6 +43,45 @@ bool Barycentric(const Vec2& Point, const Triangle& Tri)
 	return (U >= 0.0f) && (V >= 0.0f) && (U + V < 1.0f);
 }
 
+// With this method:
+//  Only V2,Dot02,Dot12,U, and V have to be re-generated for each vertex
+void BarycentricFill(qTri::Image& Frame, const qTri::Triangle& Tri)
+{
+	const Vec2 V0 = Tri.Vert[2] - Tri.Vert[0];
+	const Vec2 V1 = Tri.Vert[1] - Tri.Vert[0];
+
+	const std::uint32_t Dot00 = glm::compAdd(V0 * V0);
+	const std::uint32_t Dot01 = glm::compAdd(V0 * V1);
+	const std::uint32_t Dot11 = glm::compAdd(V1 * V1);
+
+	const glm::float32_t Det = (Dot00 * Dot11 - Dot01 * Dot01);
+
+	const auto XBounds = std::minmax({Tri.Vert[0].x, Tri.Vert[1].x, Tri.Vert[2].x});
+	const auto YBounds = std::minmax({Tri.Vert[0].y, Tri.Vert[1].y, Tri.Vert[2].y});
+	for(
+		std::size_t y = std::min<std::size_t>(YBounds.first, 0);
+		y < std::max<std::size_t>(YBounds.second, Frame.Height);
+		++y
+	)
+	{
+		for(
+			std::size_t x = std::min<std::size_t>(XBounds.first, 0);
+			x < std::max<std::size_t>(XBounds.second, Frame.Width);
+			++x
+		)
+		{
+			const Vec2 CurPoint{x, y};
+			const Vec2 V2 = CurPoint - Tri.Vert[0];
+			const std::uint32_t Dot02 = glm::compAdd(V0 * V2);
+			const std::uint32_t Dot12 = glm::compAdd(V1 * V2);
+			const glm::float32_t U = (Dot11 * Dot02 - Dot01 * Dot12) / Det;
+			const glm::float32_t V = (Dot00 * Dot12 - Dot01 * Dot02) / Det;
+			Frame.Pixels[x + y * Frame.Width] = ((U >= 0.0f) && (V >= 0.0f) && (U + V < 1.0f)) | Frame.Pixels[x + y * Frame.Width
+			];
+		}
+	}
+}
+
 template< bool TestFunc(const qTri::Vec2& Point, const qTri::Triangle& Tri) >
 void SerialBlit(qTri::Image& Frame, const qTri::Triangle& Tri)
 {
@@ -68,8 +107,9 @@ void SerialBlit(qTri::Image& Frame, const qTri::Triangle& Tri)
 const std::pair<
 	void(*)(qTri::Image& Frame, const qTri::Triangle& Tri),
 	const char*
-> FillAlgorithms[2] = {
+> FillAlgorithms[3] = {
 	{SerialBlit<CrossTest>, "Serial-CrossProduct"},
-	{SerialBlit<Barycentric>, "Serial-Barycentric"}
+	{SerialBlit<Barycentric>, "Serial-Barycentric"},
+	{BarycentricFill, "Serial-BarycentricFill"}
 };
 }
