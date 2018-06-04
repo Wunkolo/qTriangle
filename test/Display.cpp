1ab23fae8d5465c83a01df7e47c7cadc9ded00e6
Wunkolo <Wunkolo@gmail.com>
2018-06-04 16:23:09 -0400

    Add random triangle generation for Display test

--- test/Display.cpp
+++ test/Display.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <type_traits>
 #include <algorithm>
+#include <random>
 
 #include <glm/glm.hpp>
 
@@ -38,20 +39,24 @@ static const bool _WndV100Enabled = []() -> bool
 constexpr std::size_t Width = 80;
 constexpr std::size_t Height = 50;
 
+static qTri::Triangle Triangles[5];
+
 int main()
 {
-	const qTri::Triangle Triangles[] = {
-		{
-			{0, 0},
-			{Width, Height / 2},
-			{Width / 2, Height}
-		},
-		{
-			{Width, 0},
-			{Width, Height / 2},
-			{0, Height}
-		}
-	};
+	// Generate random triangles
+	std::random_device RandomDevice;
+	std::mt19937 RandomEngine(RandomDevice());
+	std::uniform_int_distribution<std::int32_t> WidthDis(0, Width);
+	std::uniform_int_distribution<std::int32_t> HeightDis(0, Height);
+	for( qTri::Triangle& CurTriangle : Triangles )
+	{
+		CurTriangle.A.x = WidthDis(RandomEngine);
+		CurTriangle.A.y = HeightDis(RandomEngine);
+		CurTriangle.B.x = WidthDis(RandomEngine);
+		CurTriangle.B.y = HeightDis(RandomEngine);
+		CurTriangle.C.x = WidthDis(RandomEngine);
+		CurTriangle.C.y = HeightDis(RandomEngine);
+	}
 	for( const auto& FillAlgorithm : qTri::FillAlgorithms )
 	{
 		std::printf(
