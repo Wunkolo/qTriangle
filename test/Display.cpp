0b58248c169057b9c7b4e288a256b3ac13503817
Wunkolo <Wunkolo@gmail.com>
2018-06-03 21:52:32 -0400

    Update vector/type resolution to 32-bit types
    Gonna make this assumption to make later SIMD-ing better

--- test/Display.cpp
+++ test/Display.cpp
@@ -37,9 +37,9 @@ using FrameBuffer = std::array<glm::float32_t, Width * Height>;
 struct Triangle
 {
 	// Clock-wise order
-	glm::u64vec2 A;
-	glm::u64vec2 B;
-	glm::u64vec2 C;
+	glm::u32vec2 A;
+	glm::u32vec2 B;
+	glm::u32vec2 C;
 };
 
 void Draw(const FrameBuffer& Frame);
@@ -76,8 +76,8 @@ void Draw(const FrameBuffer& Frame)
 							Frame[x + y * Width],
 							0.0f,
 							1.0f
-						) * (std::extent<decltype(Shades)>::value - 2)
-					)
+						) * (std::extent<decltype(Shades)>::value - 1)
+					) - 1
 				]
 			);
 		}
@@ -88,30 +88,30 @@ void Draw(const FrameBuffer& Frame)
 
 void FillTriangle(FrameBuffer& Frame, const Triangle& Tri)
 {
-	const glm::u64vec2 V0 = Tri.C - Tri.A;
-	const glm::u64vec2 V1 = Tri.B - Tri.A;
+	const glm::u32vec2 V0 = Tri.C - Tri.A;
+	const glm::u32vec2 V1 = Tri.B - Tri.A;
 
 	for( std::size_t y = 0; y < Height; ++y )
 	{
 		for( std::size_t x = 0; x < Width; ++x )
 		{
-			const glm::u64vec2 CurPoint{x, y};
-			const glm::u64vec2 V2 = CurPoint - Tri.A;
+			const glm::u32vec2 CurPoint{x, y};
+			const glm::u32vec2 V2 = CurPoint - Tri.A;
 
-			const std::uint64_t Dot00 = V0.x * V0.x + V0.y * V0.y;
-			const std::uint64_t Dot01 = V0.x * V1.x + V0.y * V1.y;
-			const std::uint64_t Dot02 = V0.x * V2.x + V0.y * V2.y;
-			const std::uint64_t Dot11 = V1.x * V1.x + V1.y * V1.y;
-			const std::uint64_t Dot12 = V1.x * V2.x + V1.y * V2.y;
+			const std::uint32_t Dot00 = V0.x * V0.x + V0.y * V0.y;
+			const std::uint32_t Dot01 = V0.x * V1.x + V0.y * V1.y;
+			const std::uint32_t Dot02 = V0.x * V2.x + V0.y * V2.y;
+			const std::uint32_t Dot11 = V1.x * V1.x + V1.y * V1.y;
+			const std::uint32_t Dot12 = V1.x * V2.x + V1.y * V2.y;
 
-			const glm::float64_t InvDenom = 1.0 / (Dot00 * Dot11 - Dot01 * Dot01);
-			const glm::float64_t U = (Dot11 * Dot02 - Dot01 * Dot12) * InvDenom;
-			const glm::float64_t V = (Dot00 * Dot12 - Dot01 * Dot02) * InvDenom;
+			const glm::float32_t InvDenom = 1.0f / (Dot00 * Dot11 - Dot01 * Dot01);
+			const glm::float32_t U = (Dot11 * Dot02 - Dot01 * Dot12) * InvDenom;
+			const glm::float32_t V = (Dot00 * Dot12 - Dot01 * Dot02) * InvDenom;
 
 			if(
-				(U >= 0.0) &&
-				(V >= 0.0) &&
-				(U + V < 1.0)
+				(U >= 0.0f) &&
+				(V >= 0.0f) &&
+				(U + V < 1.0f)
 			)
 			{
 				Frame[x + y * Width] = 1.0f;
